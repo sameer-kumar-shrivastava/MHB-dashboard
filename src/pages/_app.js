fca907af651da3045d2b8994b04799f68689ea30
Sameer Kumar Shrivastava <mail.sameershrivastava@gmail.com>
2023-11-06 15:51:44 +0530

    Added Code

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -27,7 +27,7 @@ const App = (props) => {
     <CacheProvider value={emotionCache}>
       <Head>
         <title>
-          Devias Kit
+          Devias Kits
         </title>
         <meta
           name="viewport"
