2c2006d19ad3038e44b4a6e6da3aa722f5808c77
Sameer Kumar Shrivastava <mail.sameershrivastava@gmail.com>
2023-11-10 10:41:24 +0530

    Added layouts to Userpage

--- src/pages/user/[username].js
+++ src/pages/user/[username].js
@@ -1,5 +1,6 @@
 // pages/user/[username].js
 import { useRouter } from 'next/router';
+import { Layout as DashboardLayout } from 'src/layouts/dashboard/layout';
 
 const UserPage = () => {
   const router = useRouter();
@@ -13,4 +14,9 @@ const UserPage = () => {
   );
 };
 
+UserPage.getLayout = (page) => (
+  <DashboardLayout>
+    {page}
+  </DashboardLayout>
+);
 export default UserPage;
